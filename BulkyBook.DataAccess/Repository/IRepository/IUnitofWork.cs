f369552f464bed8cd460d8b4483f6200be81b079
Norman <Norman@DESKTOP-BG76A5G>
2022-06-20 16:59:40 -0800

    Second commit, repository patter

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitofWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitofWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
+        ISP_Call SP_Call { get; }
+
+        void Save();
+    }
+}
